48f86647686dc5ccaf8007e33486372c5a33efd0
Olcay Alici <olcay_alici@hotmail.com>
2022-03-28 21:56:38 +0300

    Üçgen Alan Hesaplama Ödevi Eklendi

---
+++ Üçgen Alan Hesaplama.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        int a, b, c;
+        double cevre, u, alan;
+
+        System.out.print("1.Kenari giriniz :" );
+        a = inp.nextInt();
+
+        System.out.print("2.Kenari giriniz :" );
+        b = inp.nextInt();
+
+        System.out.print("3.Kenari giriniz :" );
+        c = inp.nextInt();
+
+        cevre = (a+b+c);
+        System.out.println("Ucgenin Cevresi :" +cevre);
+
+        u = (a+b+c) / 2;                                      // Üç𝑔𝑒𝑛𝑖𝑛 ç𝑒𝑣𝑟𝑒𝑠𝑖 = 2𝑢 ise 𝑢 = (a+b+c) / 2
+
+        alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));    // Heron formulü = Alan*Alan = 𝑢*(𝑢 − 𝑎)*(𝑢 − 𝑏)*(𝑢 − 𝑐)
+        System.out.println("Ucgenin Alani :" + alan);
+
+    }
+}
